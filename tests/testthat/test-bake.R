282280685342a621986c89d918f68f993b675f8c
cdmuir <chrisdmuir@gmail.com>
2018-12-02 14:28:48 -0500

    added unit test for temperature response

---
+++ tests/testthat/test-bake.R
@@ -0,0 +1,37 @@
+context("temperature responses")
+library(photosynthesis)
+
+test_that("baked parameters do not equal unbaked unless Temp = 25", {
+  
+  cs <- make_constants()
+  lp <- make_leafpar()
+  bp <- make_bakepar()
+  ep <- make_enviropar()
+  
+  lp$T_leaf <- set_units(298.15, "K")
+  blp <- bake(lp, bp, cs)
+  
+  blp %>%
+    as.data.frame() %>%
+    dplyr::select(tidyselect::ends_with("25")) %>%
+    colnames() %>%
+    stringr::str_remove("25$") %>%
+    glue::glue("blp${x} == blp${x}25", x = .) %>%
+    purrr::map_lgl(function(x) eval(parse(text = x))) %>%
+    all() %>%
+    expect_true()
+  
+  lp$T_leaf <- set_units(305, "K")
+  blp <- bake(lp, bp, cs)
+  
+  blp %>%
+    as.data.frame() %>%
+    dplyr::select(tidyselect::ends_with("25")) %>%
+    colnames() %>%
+    stringr::str_remove("25$") %>%
+    glue::glue("blp${x} == blp${x}25", x = .) %>%
+    purrr::map_lgl(function(x) eval(parse(text = x))) %>%
+    any() %>%
+    expect_false()
+  
+})
