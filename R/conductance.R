208e2fa0c046315706e421002d76b7ab3b7d01b6
cdmuir <chrisdmuir@gmail.com>
2022-11-07 08:29:14 -1000

    new conductance model

--- R/conductance.R
+++ R/conductance.R
@@ -1,11 +1,16 @@
-#' Conductance to CO2 (umol / (m^2 s Pa))
+#' Conductance to CO2 (mol / m^2 / s)
 #'
 #' @inheritParams A_supply
 #'
 #' @name CO2_conductance
 #'
 #' @details
-#'
+#' 
+#' \bold{Default conductance model}
+#' 
+#' The conductance model described in this section is used by default unless
+#' additional anatomical parameters described in the next section are provided.
+#' 
 #' Total conductance to CO2 is the sum of parallel conductances on the lower
 #' (\eqn{g_\mathrm{c,lower}}{gc_lower}) and upper
 #' (\eqn{g_\mathrm{c,upper}}{gc_upper}) leaf portions:
@@ -35,19 +40,103 @@
 #' Inf \tab all conductance on \bold{upper} surface/portion
 #' }
 #'
-#' The boundary layer conductances for each are calculated on the basis of mass and heat transfer (see \code{\link{.get_gbc}}).
+#' The boundary layer conductances for each are calculated on the basis of mass 
+#' and heat transfer (see \code{\link{.get_gbc}}).
 #'
 #' \tabular{lllll}{
 #' \emph{Symbol} \tab \emph{R} \tab \emph{Description} \tab \emph{Units} \tab \emph{Default}\cr
-#' \eqn{g_\mathrm{mc}}{g_mc} \tab \code{g_mc} \tab mesophyll conductance to CO2 (T_leaf) \tab \eqn{\mu}mol CO2 / (m\eqn{^2} s Pa) \tab \link[=bake]{calculated} \cr
-#' \eqn{g_\mathrm{sc}}{g_sc} \tab \code{g_sc} \tab stomatal conductance to CO2 \tab \eqn{\mu}mol CO2 / (m\eqn{^2} s Pa) \tab 4 \cr
-#' \eqn{g_\mathrm{uc}}{g_uc} \tab \code{g_uc} \tab cuticular conductance to CO2 \tab \eqn{\mu}mol CO2 / (m\eqn{^2} s Pa) \tab 0.1 \cr
-#' \eqn{k_\mathrm{mc}}{k_mc} \tab \code{k_mc} \tab partition of \eqn{g_\mathrm{mc}}{g_mc} to lower mesophyll \tab none \tab 1 \cr
-#' \eqn{k_\mathrm{sc}}{k_sc} \tab \code{k_sc} \tab partition of \eqn{g_\mathrm{sc}}{g_sc} to lower surface \tab none \tab 1 \cr
-#' \eqn{k_\mathrm{uc}}{k_uc} \tab \code{k_uc} \tab partition of \eqn{g_\mathrm{uc}}{g_uc} to lower surface \tab none \tab 1 \cr
+#' \eqn{g_\mathrm{mc}}{g_mc} \tab \code{g_mc} \tab mesophyll conductance to CO2 (T_leaf) \tab mol / m\eqn{^2} / s \tab \link[=bake]{calculated} \cr
+#' \eqn{g_\mathrm{sc}}{g_sc} \tab \code{g_sc} \tab stomatal conductance to CO2 \tab mol / m\eqn{^2} / s \tab `r dplyr::pull(photo_parameters[photo_parameters$R == "g_sc","default"])` \cr
+#' \eqn{g_\mathrm{uc}}{g_uc} \tab \code{g_uc} \tab cuticular conductance to CO2 \tab mol / m\eqn{^2} / s \tab `r dplyr::pull(photo_parameters[photo_parameters$R == "g_uc","default"])` \cr
+#' \eqn{k_\mathrm{mc}}{k_mc} \tab \code{k_mc} \tab partition of \eqn{g_\mathrm{mc}}{g_mc} to lower mesophyll \tab none \tab `r dplyr::pull(photo_parameters[photo_parameters$R == "k_mc","default"])` \cr
+#' \eqn{k_\mathrm{sc}}{k_sc} \tab \code{k_sc} \tab partition of \eqn{g_\mathrm{sc}}{g_sc} to lower surface \tab none \tab `r dplyr::pull(photo_parameters[photo_parameters$R == "k_sc","default"])` \cr
+#' \eqn{k_\mathrm{uc}}{k_uc} \tab \code{k_uc} \tab partition of \eqn{g_\mathrm{uc}}{g_uc} to lower surface \tab none \tab `r dplyr::pull(photo_parameters[photo_parameters$R == "k_uc","default"])` \cr
 #' }
 #'
+#' \bold{New conductance model}
+#' 
+#' The conductance model described in this section is implemented in 
+#' \bold{photosynthesis} (>= 2.1.0) if parameters to calculate the internal 
+#' airspace and liquid-phase conductances (\code{A_mes_A}, \code{g_liqc}) are 
+#' provided. These parameters are 1) the effective path lengths through the 
+#' lower and upper leaf internal airspaces (\code{delta_ias_lower}, 
+#' \code{delta_ias_upper}) and 2) the mesophyll area per leaf area 
+#' (\code{A_mes_A}) and liquid-phase conductance per mesophyll cell area 
+#' (\code{g_liqc}).
+#' 
+#' Two parallel diffusion pathways, one from each leaf surface, converge to a 
+#' single CO2 concentration at the mesophyll cell boundary. We use a single 
+#' liquid-phase resistance to represent the combined cell wall, plasmalemma, and 
+#' chloroplast resistances. The gas-phase resistance through boundary layer, 
+#' cuticle/stomata, and internal airspace is \eqn{r_\mathrm{gas,c}}; the 
+#' liquid-phase intracellular resistance is \eqn{r_\mathrm{i,c}}.
+#' 
+#'  \deqn{r_\mathrm{total,c} = r_\mathrm{gas,c} + r_\mathrm{i,c}}{r_total,c = r_gas,c + r_i,c}
+#'  
+#' The gas-phase resistance occurs through two parallel pathways, which we refer 
+#' to as the 'lower' and 'upper' pathways because horizontally oriented leaves 
+#' often have different anatomical properties on each surface. The gas-phase 
+#' resistance through pathway \eqn{i \in \{\textrm{lower,upper\}}} is:
+#' 
+#' \deqn{r_{\mathrm{gas,c},i} = r_{\mathrm{b,c},i} + r_{\mathrm{u+s,c},i} + r_{\mathrm{ias,c},i}}{r_gas,c,i = r_b,c,i + r_u+s,c,i + r_ias,c,i}
+#' 
+#' The subscripts \eqn{_\mathrm{b}}, \eqn{_\mathrm{u+s}}, and \eqn{_\mathrm{ias}} 
+#' denote boundary layer, cuticular + stomatal, and internal airspace, 
+#' respectively. The subscript \eqn{_\mathrm{c}} indicates we are considering 
+#' the conductance to CO2 rather than another molecular species.
+#' 
+#' Cuticular and stomatal conductances (1 / resistance) are parallel, so:
+#' 
+#' \deqn{1 / r_{\mathrm{u+s,c},i} = g_{\mathrm{u+s,c},i} = g_{\mathrm{u,c},i} + g_{\mathrm{s,c},i}}{1 / r_u+s,c,i = g_u+s,c,i = g_u,c,i + g_s,c,i}
+#' 
+#' Substituting the above expression into the equation for \eqn{r_{\mathrm{gas,c},i}}{r_gas,c,i}:
+#' 
+#' \deqn{r_{\mathrm{gas,c},i} = r_{\mathrm{b,c},i} + 1 / (g_{\mathrm{u,c},i} = g_{\mathrm{s,c},i}) + r_{\mathrm{ias,c},i}}{r_gas,c,i = r_b,c,i + 1 / (g_u,c,i + g_s,c,i) + r_ias,c,i}
+#' 
+#' The total gas-phase resistance is the inverse of the sum of the parallel 
+#' lower and upper conductances:
+#' 
+#' \deqn{1 / r_{\mathrm{gas,c}} = g_\mathrm{gas,c,lower} + g_\mathrm{gas,c,upper}}{1 / r_gas,c = g_gas,c = g_gas,c,lower + g_gas,c,upper}
+#' 
+#' The cuticular, stomatal, and mesophyll conductances can be the same or
+#' different for upper and lower. The partitioning factors \eqn{k_u} and \eqn{k_s} 
+#' divide the total cuticular and stomatal conductances, respectively, between 
+#' surfaces while keeping the total conductance constant:
+#'
+#' \deqn{g_{x,\mathrm{lower}} = g_x (1 / (1 + k_x))}{gx_lower = g_x (1 / (1 + k_x))}
+#' \deqn{g_{x,\mathrm{upper}} = g_x (k_x / (1 + k_x))}{gx_upper = g_x (k_x / (1 + k_x))}
+#' \deqn{g_x = g_{x,\mathrm{lower}} + g_{x,\mathrm{upper}}}{g_x = gx_lower + gx_upper}
+#'
+#' How the partitioning factors work: \cr
+#' \tabular{ll}{
+#' \eqn{k_x} \tab description \cr
+#' 0 \tab all conductance on \bold{lower} surface/portion \cr
+#' 0.5 \tab 2/3 conductance on \bold{lower} surface \cr
+#' 1 \tab conductance evenly divided between surfaces/portions \cr
+#' 2 \tab 2/3 conductance on \bold{upper} surface \cr
+#' Inf \tab all conductance on \bold{upper} surface/portion
+#' }
+#'
+#' The internal airspace conductance is the diffusivity of CO2 in of a given
+#' temperature and pressure divided by the effective path length:
+#' 
+#' \deqn{g_\mathrm{ias,c,lower} = D_\mathrm{c} / \delta_\mathrm{ias,lower}}{g_iasc_lower = D_c / delta_ias_lower}
+#' \deqn{g_\mathrm{ias,c,upper} = D_\mathrm{c} / \delta_\mathrm{ias,upper}}{g_iasc_ipper = D_c / delta_ias_upper}
+#' 
+#' \code{g_iasc_lower} and \code{g_iasc_upper} are calculated in the \link{bake} 
+#' function. See \code{\link[tealeaves]{.get_Dx}} for calculating \code{D_c}.
+#' 
+#' The liquid-phase intracellular resistance is given by:
+#' 
+#' \deqn{1 / r_\mathrm{i,c} = g_\mathrm{i,c} = g_\mathrm{liq,c} A_\mathrm{mes} / A}{1 / r_i,c = g_i,c = g_liq,c A_mes / A}
+#' 
+#' \eqn{g_\mathrm{liq,c}}{g_liq,c} is temperature sensitive. See \code{\link{bake}}.
+#' 
+#' The boundary layer conductances for each are calculated on the basis of mass
+#' and heat transfer (see \code{\link{.get_gbc}}).
+#' 
 #' @encoding UTF-8
+#' @md
 
 NULL
 
@@ -55,29 +144,56 @@ NULL
 #'
 #' @rdname CO2_conductance
 .get_gtc = function(pars, unitless, use_legacy_version) {
+
+  if (check_new_conductance(pars, baked = TRUE)) {
+    
+    gbc_lower = .get_gbc(pars, "lower", unitless, use_legacy_version)
+    gsc_lower = .get_gsc(pars, "lower", unitless)
+    guc_lower = .get_guc(pars, "lower", unitless)
+    
+    gbc_upper = .get_gbc(pars, "upper", unitless, use_legacy_version)
+    gsc_upper = .get_gsc(pars, "upper", unitless)
+    guc_upper = .get_guc(pars, "upper", unitless)
+    
+    g_usc_lower = guc_lower + gsc_lower
+    g_usc_upper = guc_upper + gsc_upper
+    g_gasc_lower = 1 / (1 / gbc_lower + 1 / g_usc_lower + 1 / pars$g_iasc_lower)
+    g_gasc_upper = 1 / (1 / gbc_upper + 1 / g_usc_upper + 1 / pars$g_iasc_upper)
+    g_gasc = g_gasc_lower + g_gasc_upper
+    g_ic = pars$g_liqc * pars$A_mes_A
+    g_tc = 1 / (1 / g_gasc + 1 / g_ic)
+    
+    if (!unitless) g_tc %<>% set_units(mol / m^2 / s)
+    
+    return(g_tc)
+    
+  } else {
+    
+    gbc_lower = .get_gbc(pars, "lower", unitless, use_legacy_version)
+    gmc_lower = .get_gmc(pars, "lower", unitless)
+    gsc_lower = .get_gsc(pars, "lower", unitless)
+    guc_lower = .get_guc(pars, "lower", unitless)
+    
+    gbc_upper = .get_gbc(pars, "upper", unitless, use_legacy_version)
+    gmc_upper = .get_gmc(pars, "upper", unitless)
+    gsc_upper = .get_gsc(pars, "upper", unitless)
+    guc_upper = .get_guc(pars, "upper", unitless)
+    
+    rc_lower = 1 / gmc_lower + 1 / gsc_lower + 1 / gbc_lower
+    gc_lower = 1 / rc_lower
+    gc_lower %<>% magrittr::add(guc_lower)
+    rc_upper = 1 / gmc_upper + 1 / gsc_upper + 1 / gbc_upper
+    gc_upper = 1 / rc_upper
+    gc_upper %<>% magrittr::add(guc_upper)
+    
+    g_tc = gc_lower + gc_upper
+    
+    if (!unitless) g_tc %<>% set_units(mol / m^2 / s)
+    
+    return(g_tc)
+    
+  }
   
-  gbc_lower = .get_gbc(pars, "lower", unitless, use_legacy_version)
-  gmc_lower = .get_gmc(pars, "lower", unitless)
-  gsc_lower = .get_gsc(pars, "lower", unitless)
-  guc_lower = .get_guc(pars, "lower", unitless)
-
-  gbc_upper = .get_gbc(pars, "upper", unitless, use_legacy_version)
-  gmc_upper = .get_gmc(pars, "upper", unitless)
-  gsc_upper = .get_gsc(pars, "upper", unitless)
-  guc_upper = .get_guc(pars, "upper", unitless)
-
-  rc_lower = 1 / gmc_lower + 1 / gsc_lower + 1 / gbc_lower
-  gc_lower = 1 / rc_lower
-  gc_lower %<>% magrittr::add(guc_lower)
-  rc_upper = 1 / gmc_upper + 1 / gsc_upper + 1 / gbc_upper
-  gc_upper = 1 / rc_upper
-  gc_upper %<>% magrittr::add(guc_upper)
-
-  g_tc = gc_lower + gc_upper
-
-  if (!unitless) g_tc %<>% set_units(mol / m^2 / s)
-
-  g_tc
 }
 
 #'  - g_uc: cuticular conductance to CO2
@@ -146,13 +262,6 @@ NULL
   
   surface %<>% match.arg(c("lower", "upper"))
 
-  if (check_new_conductance(pars, baked = TRUE)) {
-    g_mc = switch(
-      surface,
-      lower = 1 / (1 / pars$g_iasc_lower + 1 / (pars$A_mes_A * pars$g_liqc)),
-      upper = 1 / (1 / pars$g_iasc_upper + 1 / (pars$A_mes_A * pars$g_liqc))
-    )
-  } else {
     if (unitless) {
       g_mc = switch(
         surface,
@@ -166,7 +275,6 @@ NULL
         upper = pars$g_mc * (pars$k_mc / (set_units(1) + pars$k_mc))
       )
     }
-  } 
   
   g_mc
   
